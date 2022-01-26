26a5def8216036e0ee7d68cc3d0b8a6bba1037ee
Vanessa <dayan.basulto@reduc.edu.cu>
2022-01-26 18:12:04 -0500

    Cambiando configuracion de puerto y moviendo carpetas dentro de src

--- src/server.js
+++ src/server.js
@@ -1,5 +1,18 @@
 const express = require('express');
 
+//Initialization
 const app = express();
+
+//Settings
+app.set('port', process.env.PORT || 4000);
+
+//Middleware
+
+// Global variables
+
+// Routes
+
+// Static files
+
 
 module.exports = app;
