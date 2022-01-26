2d8ca5d1798546c5f02995fa870ed5bbfbc21c16
Vanessa <dayan.basulto@reduc.edu.cu>
2022-01-26 18:19:33 -0500

    Hello world

--- src/server.js
+++ src/server.js
@@ -1,16 +1,23 @@
 const express = require('express');
+const path = require('path');
 
 //Initialization
 const app = express();
 
 //Settings
 app.set('port', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, '/views'));
 
 //Middleware
+app.use(express.urlencoded({ extended: false }));
 
 // Global variables
 
+
 // Routes
+app.get('/', (req, res) => {
+    res.send('Hello world');
+})
 
 // Static files
 
