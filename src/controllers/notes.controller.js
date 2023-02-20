564681357db16770e4ed6d8c44cefbca0b785984
Vanessa <dayan.basulto@reduc.edu.cu>
2023-02-20 18:26:01 -0500

    Ultimos arreglos al registro de usuario y seguridad basica

--- src/controllers/notes.controller.js
+++ src/controllers/notes.controller.js
@@ -13,19 +13,23 @@ notesCtrl.createNewNote = async (req, res) => {
         title,
         description//es lo mismo con los dos puntos o solo
     });
+    newNote.user = req.user.id;
     await newNote.save();
     req.flash('success_msg', 'Note Added Successfuly');
     res.redirect('/notes');
 };
 notesCtrl.renderNotes = async (req, res) => {
-    const notes = await Note.find().lean();
+    const notes = await Note.find({user: req.user.id}).lean().sort({createdAt: 'desc'});
     res.render('notes/all-notes', { notes });
 };
 
 notesCtrl.renderEditForm = async (req, res) => {
     const note = await Note.findById(req.params.id).lean();
+    if (note.user != req.user.id) {
+        req.flash('error_msg', 'Not Authorized');
+        return res.redirect('/notes');
+    }
     res.render('notes/edit-note', { note });
-    console.log({ note })
 };
 
 notesCtrl.updateNote = async function (req, res) {
